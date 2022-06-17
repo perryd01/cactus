13f00857842714bdf0ce406b03def78d57b2d824
Miklos Daniel <daniel.miklos0@gmail.com>
2022-06-17 18:00:30 +0200

    feat: basic chia docker image without proper config

---
+++ tools/docker/chia-all-in-one/entry.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+bash install.sh && . ./activate && bash install-timelord.sh && chia init && chia keys generate
---
+++ tools/docker/chia-all-in-one/launch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+chia start all
+
+trap "echo Shutting down ...; chia stop all -d; exit 0" SIGINT SIGTERM
+
+# shellcheck disable=SC2154
+if [[ ${log_to_file} == 'true' ]]; then
+    # Ensures the log file actually exists, so we can tail successfully
+    touch "$CHIA_ROOT/log/debug.log"
+    tail -F "$CHIA_ROOT/log/debug.log" &
+fi
+
+while true; do sleep 1; done
